748b92d23b28f6aa47b7a52d961f1de29d279f20
Diego Ribeiro <dihrm2001@gmail.com>
2022-02-26 12:17:47 -0300

    cria mensagem de erro

---
+++ src/components/Error.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Error() {
+  return (
+    <div>
+      <h1>Houve um erro ao se conectar com o servidor.</h1>
+      <h1>Tente novamente em alguns instantes.</h1>
+    </div>
+  );
+}
+
+export default Error;
