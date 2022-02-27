04109e0b91fb077351244b4f1e32efee83c36b22
Diego Ribeiro <dihrm2001@gmail.com>
2022-02-27 11:23:29 -0300

    implementa filtro por nome inserido no input

---
+++ src/components/NameFilter.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { usePlanetsContext } from '../context/PlanetsContext';
+
+function NameFilter() {
+  const {
+    filters: { filterByName: { name } },
+    setNameFilter,
+  } = usePlanetsContext();
+
+  return (
+    <div>
+      <label htmlFor="nameFilter">
+        Filtrar por nome:
+        <input
+          type="text"
+          name="nameFilter"
+          id="nameFilter"
+          value={ name }
+          onChange={ ({ target }) => setNameFilter(target.value) }
+        />
+      </label>
+    </div>
+  );
+}
+
+export default NameFilter;
