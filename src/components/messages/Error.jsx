35df9dc00c316e668877f929b2b7139ef410b362
Diego Ribeiro <dihrm2001@gmail.com>
2022-03-02 15:07:21 -0300

    Error message style

--- src/components/messages/Error.jsx
+++ src/components/messages/Error.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import '../../styles/Error.css';
+
 function Error() {
   return (
-    <div>
+    <div className="error-message">
       <h1>Houve um erro ao se conectar com o servidor.</h1>
       <h1>Tente novamente em alguns instantes.</h1>
     </div>
